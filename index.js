82198a9ddec9a760534eb086655b71869f3e5ac1
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2024-04-19 23:10:35 +0300

    updates

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 
 require('dotenv').config();
 
 const app = express();
 const PORT = 3000;
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.get('/', (req, res) => res.send('Express on Vercel'));
 
